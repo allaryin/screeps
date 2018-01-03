3bbee03f1a30ef7be6fec1db1ae1427620184d9f
Ammon Lauritzen <allaryin@gmail.com>
2018-01-03 14:36:23 -0600

    Fixing hang when builders converge on a site.

--- task.build.js
+++ task.build.js
@@ -9,25 +9,37 @@ module.exports = {
 		} else {
 			creep.say('🚧');
 		}
+
+		//var err = "";
 		
 		// determine target for building
 		var target = null;
 		if( creep.memory.target != null ) {
+			//err += "0";
 			target = Game.getObjectById(creep.memory.target);
+			if( target == null ) {
+				creep.memory.target = null;
+				return;
+			}
 		} else {
+			//err += "1";
 			target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
 			if( target != null ) {
 				creep.memory.target = target.id;
 			} else {
 				// we were unable to find a target to build, cycle tasks...
 				creep.memory.task = "repair";
+				creep.memory.target = null;
+				return;
 			}
 		}
 
 		// try to build
 		if( target != null ) {
+			//err += "2";
 			var res = creep.build(target);
 			if( res == ERR_NOT_IN_RANGE) {
+				//err += "3";
 				creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
 			} else if( res == OK) {
 				var progress = Math.floor(100.0 * target.progress / target.progressTotal);
@@ -35,6 +47,11 @@ module.exports = {
 			} else {
 				creep.say(res);
 			}
+		} else {
+			//err += "4";
+			console.log(creep+": is trying to build null structure");
 		}
+
+		//console.log(creep,err);
 	}
 };
